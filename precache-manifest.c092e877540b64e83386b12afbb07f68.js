9f8727b187ba3ea8ff9846782facd9a3a932f284
Kalita Mikhail <mkalita1991@gmail.com>
2019-04-26 09:43:38 +0300

    Updates

---
+++ precache-manifest.c092e877540b64e83386b12afbb07f68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8906429a7746850302bd",
+    "url": "/labinventtest/static/js/runtime~main.6633ec85.js"
+  },
+  {
+    "revision": "a86606d053ecd342ebb4",
+    "url": "/labinventtest/static/js/main.2db6a4aa.chunk.js"
+  },
+  {
+    "revision": "98ffda36c1a3c8414489",
+    "url": "/labinventtest/static/js/2.34a40746.chunk.js"
+  },
+  {
+    "revision": "a86606d053ecd342ebb4",
+    "url": "/labinventtest/static/css/main.e56cb22a.chunk.css"
+  },
+  {
+    "revision": "9a5450bfe6f9fe1a7a7c13e977fdd7ff",
+    "url": "/labinventtest/index.html"
+  }
+];
